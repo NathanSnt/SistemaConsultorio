d1f16820febab99c9843fb47cf69b3bed2ece282
Nathan Santos <natan-silva-2011@hotmail.com>
2023-02-10 17:14:24 -0300

    Adicionado campos de dados sobre médicos.

--- Consultorio/frmMedicos.Designer.cs
+++ Consultorio/frmMedicos.Designer.cs
@@ -36,7 +36,20 @@ namespace Consultorio
             this.btnCadastrar = new System.Windows.Forms.Button();
             this.btnExcluir = new System.Windows.Forms.Button();
             this.btnAlterar = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblCRM = new System.Windows.Forms.Label();
+            this.lblEspecialidade = new System.Windows.Forms.Label();
+            this.cbbEspecialidade = new System.Windows.Forms.ComboBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.mskCRM = new System.Windows.Forms.MaskedTextBox();
+            this.gpbMedico = new System.Windows.Forms.GroupBox();
+            this.mskTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            this.gpbMedico.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -121,11 +134,138 @@ namespace Consultorio
             this.btnAlterar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnAlterar.UseVisualStyleBackColor = true;
             // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(98, 56);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(419, 26);
+            this.txtNome.TabIndex = 2;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(94, 33);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(51, 20);
+            this.lblNome.TabIndex = 3;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblCRM
+            // 
+            this.lblCRM.AutoSize = true;
+            this.lblCRM.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCRM.Location = new System.Drawing.Point(6, 33);
+            this.lblCRM.Name = "lblCRM";
+            this.lblCRM.Size = new System.Drawing.Size(45, 20);
+            this.lblCRM.TabIndex = 5;
+            this.lblCRM.Text = "CRM";
+            // 
+            // lblEspecialidade
+            // 
+            this.lblEspecialidade.AutoSize = true;
+            this.lblEspecialidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEspecialidade.Location = new System.Drawing.Point(6, 197);
+            this.lblEspecialidade.Name = "lblEspecialidade";
+            this.lblEspecialidade.Size = new System.Drawing.Size(108, 20);
+            this.lblEspecialidade.TabIndex = 7;
+            this.lblEspecialidade.Text = "Especialidade";
+            // 
+            // cbbEspecialidade
+            // 
+            this.cbbEspecialidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbbEspecialidade.FormattingEnabled = true;
+            this.cbbEspecialidade.Location = new System.Drawing.Point(10, 220);
+            this.cbbEspecialidade.Name = "cbbEspecialidade";
+            this.cbbEspecialidade.Size = new System.Drawing.Size(273, 28);
+            this.cbbEspecialidade.TabIndex = 5;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(6, 119);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(48, 20);
+            this.lblEmail.TabIndex = 10;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(10, 142);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(374, 26);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefone.Location = new System.Drawing.Point(386, 119);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(71, 20);
+            this.lblTelefone.TabIndex = 12;
+            this.lblTelefone.Text = "Telefone";
+            // 
+            // mskCRM
+            // 
+            this.mskCRM.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mskCRM.Location = new System.Drawing.Point(10, 56);
+            this.mskCRM.Mask = "00000000";
+            this.mskCRM.Name = "mskCRM";
+            this.mskCRM.Size = new System.Drawing.Size(82, 26);
+            this.mskCRM.TabIndex = 1;
+            this.mskCRM.ValidatingType = typeof(int);
+            // 
+            // gpbMedico
+            // 
+            this.gpbMedico.Controls.Add(this.button1);
+            this.gpbMedico.Controls.Add(this.mskTelefone);
+            this.gpbMedico.Controls.Add(this.lblCRM);
+            this.gpbMedico.Controls.Add(this.lblTelefone);
+            this.gpbMedico.Controls.Add(this.mskCRM);
+            this.gpbMedico.Controls.Add(this.txtNome);
+            this.gpbMedico.Controls.Add(this.lblEmail);
+            this.gpbMedico.Controls.Add(this.txtEmail);
+            this.gpbMedico.Controls.Add(this.lblNome);
+            this.gpbMedico.Controls.Add(this.cbbEspecialidade);
+            this.gpbMedico.Controls.Add(this.lblEspecialidade);
+            this.gpbMedico.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpbMedico.Location = new System.Drawing.Point(26, 43);
+            this.gpbMedico.Name = "gpbMedico";
+            this.gpbMedico.Size = new System.Drawing.Size(717, 365);
+            this.gpbMedico.TabIndex = 0;
+            this.gpbMedico.TabStop = false;
+            this.gpbMedico.Text = "Informações do médico";
+            // 
+            // mskTelefone
+            // 
+            this.mskTelefone.Location = new System.Drawing.Point(390, 142);
+            this.mskTelefone.Mask = "(00) 00000-0000";
+            this.mskTelefone.Name = "mskTelefone";
+            this.mskTelefone.Size = new System.Drawing.Size(127, 26);
+            this.mskTelefone.TabIndex = 4;
+            this.mskTelefone.ValidatingType = typeof(int);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(289, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 28);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "&Inserir";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // frmMedicos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(780, 557);
+            this.Controls.Add(this.gpbMedico);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -135,6 +275,8 @@ namespace Consultorio
             this.Text = "Consultório Médico";
             this.Load += new System.EventHandler(this.frmMedicos_Load);
             this.panel1.ResumeLayout(false);
+            this.gpbMedico.ResumeLayout(false);
+            this.gpbMedico.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -147,5 +289,17 @@ namespace Consultorio
         private System.Windows.Forms.Button btnCadastrar;
         private System.Windows.Forms.Button btnExcluir;
         private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblCRM;
+        private System.Windows.Forms.Label lblEspecialidade;
+        private System.Windows.Forms.ComboBox cbbEspecialidade;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.MaskedTextBox mskCRM;
+        private System.Windows.Forms.GroupBox gpbMedico;
+        private System.Windows.Forms.MaskedTextBox mskTelefone;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Consultorio/frmMedicos.cs
+++ Consultorio/frmMedicos.cs
@@ -39,7 +39,23 @@ namespace Consultorio
             IntPtr hMenu = GetSystemMenu(this.Handle, false);
             int MenuCount = GetMenuItemCount(hMenu) - 1;
             RemoveMenu(hMenu, MenuCount, MF_BYCOMMAND);
+            carregaCombo();
+        }
+        //Criando método para carregar a combobox especialidades
+        public void carregaCombo()
+        {
+            cbbEspecialidade.Items.Add("Clínico Geral");
+            cbbEspecialidade.Items.Add("Cardiologista");
+            cbbEspecialidade.Items.Add("Anestesiologista");
+            cbbEspecialidade.Items.Add("Cirurgião Cardiovascular");
+            cbbEspecialidade.Items.Add("Cirurgião de Mão");
+            cbbEspecialidade.Items.Add("Cirurgião de Cabeça e Pescoço");
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmEspecialidade abrir = new frmEspecialidade();
+            abrir.ShowDialog();
         }
     }
 }
